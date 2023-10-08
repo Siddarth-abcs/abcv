ece7f6056f6e343f5241aed609152800f12891dd
Developer Sid <142804820+Siddarth-abcs@users.noreply.github.com>
2023-10-08 09:18:22 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,18 +1,20 @@
 import firebase from 'firebase/compat/app';
-// import 'firebase/compat/auth';
+import 'firebase/compat/auth';
 // import 'firebase/compat/firestore';
 // import "firebase/compat/storage";
 
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBjllZ17q3_sy5Ae6vV6u6J_IVjiNb0aKc",
-    authDomain: "project-93474.firebaseapp.com",
-    projectId: "project-93474",
-    storageBucket: "project-93474.appspot.com",
-    messagingSenderId: "238914722711",
-    appId: "1:238914722711:web:4e968c34d01736612f82d2",
-    measurementId: "G-TKDC6WJQN2"
-  };
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  databaseURL: import.meta.env.VITE_databaseURL,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
+};
   
 const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;
